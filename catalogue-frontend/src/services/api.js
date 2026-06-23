ffb5c595d1a3d401e707e258205bb0ff33edea0c
yanadumanska <nehidnytsya@icloud.com>
2026-06-23 13:20:56 +0300

    feat: aws config

--- catalogue-frontend/src/services/api.js
+++ catalogue-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1',
   headers: {
     'Content-Type': 'application/json',
     'Cache-Control': 'no-cache',
